cc6bcc931362a8715c5df2ff442fecb4961ca1be
Dmitrii Benenson <71938599+demonben@users.noreply.github.com>
2021-04-18 17:20:39 +0300

    did sql base

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,48 @@
 const express = require('express');
 const { readAnimals, addAnimal } = require('../data/animals');
+const { pool } = require('../lib/db')
 
 const { NewAnimalsValidateSchema } = require('./animalsShemas')
 const getValidationMiddleware = require('../middlewares/validation');
-const { v4:uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 const router = express.Router()
+
+pool.query(`CREATE TABLE IF NOT EXISTS products(
+    id            VARCHAR(36) DEFAULT(UUID()),
+    name          VARCHAR(200) NOT NULL,
+    price         INT NOT NULL,
+    created_date  DATE DEFAULT(CURRENT_DATE),
+    PRIMARY KEY(id)
+);`,(err)=>{
+    if(err){
+        console.error(err);
+    }else{
+        console.log("product table created successfully")
+    }
+}
+)
 
 router.get("/", async (req, res, next) => {
     try {
         const animals = await readAnimals()
-        console.log(animals)
+        // console.log(animals)
         res.send({ animals });
     } catch (err) {
         next(err)
     }
 });
-router.post("/", getValidationMiddleware(NewAnimalsValidateSchema), async(req, res, next) => {
-    try{
-        const { nameAnimal} = req.body;
+router.post("/", getValidationMiddleware(NewAnimalsValidateSchema), async (req, res, next) => {
+    try {
+        const { nameAnimal } = req.body;
         const newAnimal = {
             id: uuid(),
             nameAnimal,
             dateCreated: Date.now(),
         }
         await addAnimal(newAnimal);
-        
-        res.status(201).send({ animal: newAnimal});
-    }catch(err){
+
+        res.status(201).send({ animal: newAnimal });
+    } catch (err) {
         next(err)
     }
 })
